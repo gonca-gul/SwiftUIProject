340efe4bf03f443cf8b999e23feca52e69169762
Gonca Gül <73940570+gonca-gul@users.noreply.github.com>
2024-01-29 11:39:24 +0300

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Gonca Gül on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
